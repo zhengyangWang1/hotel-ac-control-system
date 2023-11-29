0861398308cd7f8e6bc4fc81e6ae4a1ba83d0175
zhengyangWang1 <wangzhengyang@bupt.edu.cn>
2023-11-29 10:36:03 +0800

    用户端views完善，README改为代码描述和需求

--- Django/ac_control/user/views.py
+++ Django/ac_control/user/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, redirect
 from user import models
 import datetime
 from manager.views import Scheduler
+from django.http import JsonResponse
 
 # Create your views here.
 
 scheduler = Scheduler()  # 创建一个调度器
+
 
 def register(request):
     if request.method == 'POST':
@@ -31,7 +33,7 @@ def login_room(request):
 
         except models.User.DoesNotExist:
             # 如果用户不存在或密码不匹配，返回登录页面或其他提示页面
-            return render(request, 'login.html')
+            return render(request, 'login.html', '登陆失败')
 
     return render(request, 'login.html')
 
@@ -42,11 +44,11 @@ def login(request):
 
 # 用户点击开机
 def open_ac(request):  # 在点击开启空调后执行： 加入调度队列-->判断调度状态-->如果进入服务队列，开启空调
-                                                                  # -->如果需要等待资源，显示等候中
+    # -->如果需要等待资源，显示等候中
     """获取request中的用户信息和空调数据"""
-    user_id = request.POST.get('user_id')
+
     room_id = request.POST.get('room_id')
-    room = scheduler.request_on(room_id, 26)  # 加入调度队列
+    room = scheduler.request_on(room_id, 26)  # 加入调度队列，返回一个房间对象，包含状态（1：服务，2：等待）
 
     if room.state == 1:
         # 打开空调
@@ -59,24 +61,26 @@ def open_ac(request):  # 在点击开启空调后执行： 加入调度队列-->
     return render(request, 'tem_c2.html', context)
 
 
+# 如果空调为等待状态，需要一个函数，监控空调状态，当状态变为服务时，将其加入服务队列，同时将前端状态相应改变
+def change_ac_state(request):
+    room_id = request.POST.get('room_id')
+    room = models.Room.objects.get(room_id=room_id)
+    new_state = room.state  # 获取当前房间状态
+    return JsonResponse({'room_id': room_id, 'new_state': new_state})
+
+
 # 用户点击关机
 def close_ac(request):
     """把room对象从room_list中移除"""
-    user_id = request.POST.get('user_id')
     room_id = request.POST.get('room_id')
-    temp = request.POST.get('temp')
-    wind_speed = request.POST.get('wind_speed')
-    # 将AirCondition中将该房间空调的信息删去
-    models.AirCondition.objects.filter(room_id=room_id).delete()
-    # 在Room中添加一条操作信息，表明空调已经关闭
-    models.Room.objects.create(room_id=room_id, user_id=user_id, temp=temp, wind_speed=wind_speed,
-                               close_time=datetime.datetime.now())
-    return render(request, '关机后显示已关机')
+    room = scheduler.request_off(room_id)
+    room_state = '关机'
+    context = {'room_state': room_state}
+    return render(request, 'tem_c2.html', context)
 
 
 # 用户设定好温度和风速后点击确定
 def change_temp_wind(request):
-    user_id = request.POST.get('user_id')
     room_id = request.POST.get('room_id')
     temp = request.POST.get('temp')
     wind_speed = request.POST.get('wind_speed')
@@ -84,8 +88,5 @@ def change_temp_wind(request):
     ac = models.AirCondition.objects.get(room_id=room_id)
     ac.temp = temp
     ac.wind_speed = wind_speed
-    ac.save()
-    # 在Room中添加一条操作，表明更改后的空调参数
-    models.Room.objects.create(room_id=room_id, user_id=user_id, temp=temp, wind_speed=wind_speed,
-                               change_time=datetime.datetime.now())
-    return render(request, '显示更改成功')
+    ac.save()  # 更新room表中的信息
+    return render(request, 'tem_c2.html')

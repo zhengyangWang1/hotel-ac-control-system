ebdd9fa2df2328864f99028ccfb1507962db3628
jenny-1979 <2979254568@qq.com>
2023-11-30 15:14:23 +0800

    script_temC2中添加定时更新空调状态函数

--- Django/ac_control/user/static/script_temc2.js
+++ Django/ac_control/user/static/script_temc2.js
@@ -129,3 +129,26 @@ function toggleAC() {
         });
     }
 });
+
+
+function updateStatus() {
+    // 使用fetch或其他AJAX方法发送请求
+    fetch('后端状态更新URL')
+        .then(response => response.json())
+        .then(data => {
+            // 假设服务器返回的数据格式是 { cur_tem: '...', cur_wind: '...', cost: '...', sum_cost: '...', ac_status: '...' }
+            document.getElementById('cur_tem').textContent = data.cur_tem;
+            document.getElementById('cur_wind').textContent = data.cur_wind;
+            document.getElementById('cost').textContent = data.cost;
+            document.getElementById('sum_cost').textContent = data.sum_cost;
+
+            // 更新空调状态
+            document.getElementById('ac-status-text').textContent = `空调已${data.ac_status}`;
+
+            // 其他更新逻辑...
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// 设置定时器，每30秒调用一次updateStatus函数
+setInterval(updateStatus, 30000);
